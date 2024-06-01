d8585e60b21c5f27dee475f253fe7d5983484559
quangthaiifi <nqthaicntttn@gmail.com>
2024-06-01 08:50:46 +0700

    remove import file SecurePage in test.e2e.js

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,6 +1,5 @@
 const { expect } = require('@wdio/globals')
 const HomePage = require('../pageobjects/HomePage')
-const SecurePage = require('../pageobjects/secure.page')
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         HomePage.open();
